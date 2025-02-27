0a3b447be9635109d190c652773e5eb8fc3d67c0
jfariza07 <fariza_jek@icloud.com>
2025-02-27 20:21:42 +0500

    generators.py

--- generators.py
+++ generators.py
@@ -1,3 +1,4 @@
+#task1
 def squares_up_to_n(N):
     for i in range(N + 1):
         yield i * i
@@ -5,3 +6,41 @@ def squares_up_to_n(N):
 # Example usage
 for num in squares_up_to_n(5):
     print(num, end=" ")
+
+
+#task2
+def even_numbers(n):
+    for i in range(0, n + 1, 2):
+        yield i
+
+n = int(input("Enter a number: "))
+print(",".join(map(str, even_numbers(n))))
+
+
+#task3
+def divisible_by_3_and_4(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+
+for num in divisible_by_3_and_4(50):
+    print(num, end=" ")
+
+
+#task4
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i * i
+
+for val in squares(3, 7):
+    print(val)
+
+
+#task5
+def countdown(n):
+    for i in range(n, -1, -1):
+        yield i
+
+for num in countdown(10):
+    print(num, end=" ")
+
