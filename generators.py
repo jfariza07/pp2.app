f08bc84be10bfbe5732e133b3046c6e514571275
jfariza07 <fariza_jek@icloud.com>
2025-03-01 13:56:24 +0500

    generators.py

--- generators.py
+++ generators.py
@@ -1,45 +1,39 @@
 #task1
-def squares_up_to_n(N):
+def s(N):
+    result = []
     for i in range(N + 1):
-        yield i * i
+        result.append(i * i) 
+    return result
 
-for num in squares_up_to_n(5):
-    print(num, end=" ")
+print(s(5))
 
 
 #task2
-def even_numbers(n):
-    for i in range(0, n + 1, 2):
-        yield i
-
-n = int(input("Enter a number: "))
-print(",".join(map(str, even_numbers(n))))
+def even(n):
+    return [i for i in range(0, n + 1, 2)]
+n = int(input())
+print(",".join(map(str, even(n))))
 
 
 #task3
-def divisible_by_3_and_4(n):
-    for i in range(n + 1):
-        if i % 3 == 0 and i % 4 == 0:
-            yield i
+def divisible(n):
+    result = [i for i in range(n + 1) if i % 3 == 0 and i % 4 == 0]
+    return result
 
-for num in divisible_by_3_and_4(50):
-    print(num, end=" ")
+print(*divisible(50))
 
 
 #task4
-def squares(a, b):
-    for i in range(a, b + 1):
-        yield i * i
-
-for val in squares(3, 7):
-    print(val)
+#s-平方
+#m-循环的值
+def s(a, b):
+    return [i * i for i in range(a, b + 1)]
+for m in s(3, 7):  
+    print(m)  
 
 
 #task5
+#countdown-倒计时
 def countdown(n):
-    for i in range(n, -1, -1):
-        yield i
-
-for num in countdown(10):
-    print(num, end=" ")
-
+    return list(range(n, -1, -1))
+print(*countdown(10))
