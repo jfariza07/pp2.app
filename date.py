db7227ec0a7a95eb22db0d54e08f0d883876cb31
jfariza07 <fariza_jek@icloud.com>
2025-03-01 13:36:43 +0500

    date.py

--- date.py
+++ date.py
@@ -1,35 +1,35 @@
 #task1
 from datetime import datetime, timedelta
 
-today = datetime.today()
-five_days_ago = today - timedelta(days=5)
-print("Five days ago:", five_days_ago.date())
+td = datetime.today()
+five = td - timedelta(days=5)
+print(five.date())
 
 
 #task2
 from datetime import datetime, timedelta
 
-today = datetime.today()
-yesterday = today - timedelta(days=1)
-tomorrow = today + timedelta(days=1)
+td = datetime.today()
+ye = td - timedelta(days=1)
+tm = td + timedelta(days=1)
 
-print("Yesterday:", yesterday.date())
-print("Today:", today.date())
-print("Tomorrow:", tomorrow.date())
+print(ye.date())
+print(td.date())
+print(tm.date())
 
 
 #task3
 from datetime import datetime
 
 now = datetime.now().replace(microsecond=0)
-print("Datetime without microseconds:", now)
+print(now)
 
 
 #task4
 from datetime import datetime
 
-date1 = datetime(2024, 2, 20, 12, 0, 0)  # Example date
-date2 = datetime(2024, 2, 27, 12, 0, 0)  # Another example date
+d1 = datetime(2025, 2, 23, 18, 0, 0)
+d2 = datetime(2025, 2, 25, 18, 0, 0)
 
-difference = abs((date2 - date1).total_seconds())
-print("Difference in seconds:", difference)
+difference = abs((d2 - d1).total_seconds())
+print(difference)
